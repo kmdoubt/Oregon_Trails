42999c33210fd240481f24b235f314f68c2a6c06
kmdoubt <115440666+kmdoubt@users.noreply.github.com>
2022-10-11 16:20:02 -0400

    Delete Oregon Trails.py

--- Oregon Trails.py
+++
@@ -1,7 +0,0 @@
-#---------Oregon Trails----------
-
-print('You are traveling the Oregon Trails')
-a1 = input('Did you bring a wife or a horse with you?')
-inventory = {'romance': 'single', 'transportation': 'foot', 'clothing': 'peasant\'\s clothing', 'items': 'nothing'}
-if (a1 == 'wife'):
-    x = 1
